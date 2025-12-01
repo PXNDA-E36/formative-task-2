949c1b7770519a910f62cd3acf9c4fccf5933995
Abdul Abbasi <asabbasi2006.2@gmail.com>
2025-12-01 15:50:31 +0000

    added init function + fixed bugs in light and input.track functions

--- src/main/java/FormativeTask2.java
+++ src/main/java/FormativeTask2.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
 
 import swiftbot.*;
+// import com.pi4j.io.g
 
 public class FormativeTask2 {
     static SwiftBotAPI swiftbot;
+    static char[] colours = { 'R', 'G', 'B', 'W' };
 
     private static class sequence {
         static ArrayList<Character> sequence = new ArrayList<Character>();
-        static char[] colours = { 'R', 'G', 'B', 'W' };
 
         private static char[] generate() {
+            // char[] colours = { 'R', 'G', 'B', 'W' };
+
             for (int i = 0; i < 5; i++) {
                 sequence.add(colours[(int) (Math.random() * colours.length)]);
             }
@@ -25,7 +28,7 @@ public class FormativeTask2 {
 
     }
 
-    private static void lightButtons(char c) {
+    private static void light(char c) {
         int[] red = new int[] { 255, 0, 0 };
         int[] blue = new int[] { 0, 0, 255 };
         int[] green = new int[] { 0, 255, 0 };
@@ -34,14 +37,19 @@ public class FormativeTask2 {
         switch (c) {
             case 'R':
                 swiftbot.fillUnderlights(red);
+                break;
             case 'G':
                 swiftbot.fillUnderlights(green);
+                break;
             case 'B':
                 swiftbot.fillUnderlights(blue);
+                break;
             case 'W':
                 swiftbot.fillUnderlights(white);
+                break;
             default:
                 swiftbot.disableUnderlights();
+                break;
         }
     }
 
@@ -52,16 +60,11 @@ public class FormativeTask2 {
         static Button buttons[] = { Button.A, Button.B, Button.X, Button.Y };
 
         private static boolean track(Button correct) {
-            // swiftbot.enableButton(Button.A, () -> handlePress(Button.A, button));
-            //
-            // swiftbot.enableButton(Button.B, () -> handlePress(Button.B, button));
-            //
-            // swiftbot.enableButton(Button.X, () -> handlePress(Button.X, button));
-            //
-            // swiftbot.enableButton(Button.Y, () -> handlePress(Button.Y, button));
+            result = null;
+            swiftbot.disableAllButtons();
 
             for (Button button : buttons) {
-                swiftbot.enableButton(button, () -> handlePress(button, correct));
+                swiftbot.enableButton(button, () -> handle(button, correct));
             }
 
             synchronized (lock) {
@@ -78,13 +81,64 @@ public class FormativeTask2 {
             return result;
         }
 
-        private static void handlePress(Button input, Button correct) {
+        private static void handle(Button input, Button correct) {
             synchronized (lock) {
                 result = (input == correct);
                 lock.notify();
             }
 
             swiftbot.disableButton(input);
+        }
+    }
+
+    private static void init() {
+        for (Character c : colours) {
+            boolean correct;
+            switch (c) {
+                case 'R':
+                    System.out.println("Red is assigned to button A");
+                    light('R');
+                    System.out.println("Press A to ack");
+                    swiftbot.setButtonLight(Button.A, true);
+                    correct = input.track(Button.A);
+                    if (correct) {
+                        swiftbot.disableButtonLights();
+                        break;
+                    }
+                case 'G':
+                    System.out.println("Green is assigned to button B");
+                    light('G');
+                    System.out.println("Press B to ack");
+                    swiftbot.setButtonLight(Button.B, true);
+                    correct = input.track(Button.B);
+                    if (correct) {
+                        swiftbot.disableButtonLights();
+                        break;
+                    }
+                case 'B':
+                    System.out.println("Blue is assigned to button X");
+                    light('B');
+                    System.out.println("Press X to ack");
+                    swiftbot.setButtonLight(Button.X, true);
+                    correct = input.track(Button.X);
+                    if (correct) {
+                        swiftbot.disableButtonLights();
+                        break;
+                    }
+                case 'W':
+                    System.out.println("White is assigned to button Y");
+                    light('W');
+                    System.out.println("Press Y to ack");
+                    swiftbot.setButtonLight(Button.Y, true);
+                    correct = input.track(Button.Y);
+                    if (correct) {
+                        swiftbot.disableButtonLights();
+                        break;
+                    }
+                default:
+                    break;
+            }
+            swiftbot.disableUnderlights();
         }
     }
 
@@ -105,7 +159,5 @@ public class FormativeTask2 {
         swiftbot = SwiftBotAPI.INSTANCE;
 
         System.out.println("starting...");
-
-        input.track(Button.A);
     }
 }
